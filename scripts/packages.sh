cccc0f61207b3a61d73ef0f801c4fd26e3fdb24f
Baptiste MORIN <1932256+morinb@users.noreply.github.com>
2026-05-08 18:14:43 +0000

    typo

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -2,7 +2,7 @@
 
 install_apt_packages() {
     run_cmd "curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/debian.griffo.io.gpg"
-    run_cmd 'echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | sudo tee /etc/apt/sources.list.d/debian.griffo.io.list"'
+    run_cmd 'echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | sudo tee /etc/apt/sources.list.d/debian.griffo.io.list'
 
 
     run_cmd "sudo apt update"
