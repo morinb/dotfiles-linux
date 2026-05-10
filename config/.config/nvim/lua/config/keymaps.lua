2d8d9e7f35dc55528b6c36ee115453bfa06e1f13
Baptiste MORIN <1932256+morinb@users.noreply.github.com>
2026-05-10 17:31:17 +0200

    nvim keymap

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,25 @@
+-- Modes :
+-- -- "n": normal mode
+-- -- "v": visual mode
+-- -- "x": visual_block mode
+-- -- "i": insert mode
+-- -- "t": terminal mode
+-- -- "c": command mode
+
+
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = ","
 
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
+
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", {noremap=true, silent=true})
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", {noremap=true, silent=true})
+
+vim.keymap.set("v", "<", "<gv", {noremap=true, silent=true}) -- Right Indentation
+vim.keymap.set("v", ">", ">gv", {noremap=true, silent=true}) -- Left Indentation
+
+-- Save
+vim.keymap.set("i", "<C-s>", "<esc>:w<cr>", {noremap=true, silent=true})
+-- select all content
+vim.keymap.set("n", "<C-a>", "ggVG", {noremap=true, silent=true})
